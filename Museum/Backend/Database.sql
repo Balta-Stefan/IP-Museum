c74084ce4825cc23799675749af947f451fa5ff6
Balta-Stefan <pivad@protonmail.ch>
2022-02-14 11:20:49 +0100

    Added database generation script.

---
+++ Museum/Backend/Database.sql
@@ -0,0 +1,149 @@
+-- MySQL Workbench Forward Engineering
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+
+-- -----------------------------------------------------
+-- Schema museum
+-- -----------------------------------------------------
+
+-- -----------------------------------------------------
+-- Schema museum
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `museum` DEFAULT CHARACTER SET utf8 ;
+USE `museum` ;
+
+-- -----------------------------------------------------
+-- Table `museum`.`Museums`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `museum`.`Museums` (
+  `museumID` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(45) NOT NULL,
+  `address` VARCHAR(45) NOT NULL,
+  `phone` VARCHAR(45) NOT NULL,
+  `city` VARCHAR(45) NOT NULL,
+  `country` VARCHAR(45) NOT NULL,
+  `latitude` DECIMAL(10,8) NOT NULL,
+  `longitude` DECIMAL(10,8) NOT NULL,
+  `type` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`museumID`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `museum`.`Users`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `museum`.`Users` (
+  `userID` INT NOT NULL AUTO_INCREMENT,
+  `firstName` VARCHAR(45) NOT NULL,
+  `lastName` VARCHAR(45) NOT NULL,
+  `email` VARCHAR(45) NOT NULL,
+  `username` VARCHAR(45) NOT NULL,
+  `password` VARCHAR(255) NOT NULL,
+  `role` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`userID`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `museum`.`Tours`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `museum`.`Tours` (
+  `tourID` INT NOT NULL AUTO_INCREMENT,
+  `museum` INT NOT NULL,
+  `startTimestamp` DATETIME NOT NULL,
+  `endTimeStamp` DATETIME NOT NULL,
+  `price` DECIMAL(10,4) NOT NULL,
+  PRIMARY KEY (`tourID`),
+  INDEX `museum_idx` (`museum` ASC) VISIBLE,
+  CONSTRAINT `museum`
+    FOREIGN KEY (`museum`)
+    REFERENCES `museum`.`Museums` (`museumID`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `museum`.`TourPurchases`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `museum`.`TourPurchases` (
+  `purchaseID` BINARY(16) NOT NULL,
+  `tour` INT NOT NULL,
+  `user` INT NOT NULL,
+  `purchased` DATETIME NOT NULL,
+  PRIMARY KEY (`purchaseID`),
+  INDEX `tour_idx` (`tour` ASC) VISIBLE,
+  INDEX `user_idx` (`user` ASC) VISIBLE,
+  CONSTRAINT `tour_purchases_tour_FK`
+    FOREIGN KEY (`tour`)
+    REFERENCES `museum`.`Tours` (`tourID`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `tour_purchases_user_FK`
+    FOREIGN KEY (`user`)
+    REFERENCES `museum`.`Users` (`userID`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `museum`.`TourPictures`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `museum`.`TourPictures` (
+  `pictureID` INT NOT NULL AUTO_INCREMENT,
+  `pictureURI` VARCHAR(255) NOT NULL,
+  `tour` INT NOT NULL,
+  `type` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`pictureID`),
+  INDEX `tour_idx` (`tour` ASC) VISIBLE,
+  CONSTRAINT `tour_pictures_tour_FK`
+    FOREIGN KEY (`tour`)
+    REFERENCES `museum`.`Tours` (`tourID`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `museum`.`TourVideos`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `museum`.`TourVideos` (
+  `videoID` INT NOT NULL AUTO_INCREMENT,
+  `videoURI` VARCHAR(255) NOT NULL,
+  `type` VARCHAR(45) NOT NULL,
+  `tour` INT NOT NULL,
+  PRIMARY KEY (`videoID`),
+  INDEX `tour_idx` (`tour` ASC) VISIBLE,
+  CONSTRAINT `tour_videos_tour_FK`
+    FOREIGN KEY (`tour`)
+    REFERENCES `museum`.`Tours` (`tourID`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `museum`.`AccessTokens`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `museum`.`AccessTokens` (
+  `token` BINARY(16) NOT NULL,
+  `user` INT NOT NULL,
+  `created` DATETIME NOT NULL,
+  `validUntil` DATETIME NOT NULL,
+  `valid` TINYINT NOT NULL,
+  PRIMARY KEY (`token`),
+  INDEX `user_idx` (`user` ASC) VISIBLE,
+  CONSTRAINT `access_tokens_user_FK`
+    FOREIGN KEY (`user`)
+    REFERENCES `museum`.`Users` (`userID`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
