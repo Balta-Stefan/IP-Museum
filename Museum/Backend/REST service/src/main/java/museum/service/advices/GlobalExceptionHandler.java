0eafd8422cd68c9e56c691d1027ee48687514b99
Balta-Stefan <pivad@protonmail.ch>
2022-02-14 15:11:59 +0100

    Added bean validation exception handlers to museum and VirtualBank projects.

--- Museum/Backend/REST service/src/main/java/museum/service/advices/GlobalExceptionHandler.java
+++ Museum/Backend/REST service/src/main/java/museum/service/advices/GlobalExceptionHandler.java
@@ -3,12 +3,31 @@ package museum.service.advices;
 import museum.service.exceptions.HttpException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @ControllerAdvice
 public class GlobalExceptionHandler
 {
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Object> handleBeanValidationExceptions(MethodArgumentNotValidException exc)
+    {
+        Map<String, String> errors = new HashMap<>();
+        exc.getBindingResult().getAllErrors().forEach((err) ->
+        {
+            String fieldName = ((FieldError)err).getField();
+            String errorMsg = err.getDefaultMessage();
+            errors.put(fieldName, errorMsg);
+        });
+
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+    }
+
     @ExceptionHandler(HttpException.class)
     public ResponseEntity<Object> handleHttpException(HttpException e)
     {
