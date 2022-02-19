cfc07b472c55134cf65db68bfde0b542ff2cb76d
Balta-Stefan <pivad@protonmail.ch>
2022-02-19 11:10:58 +0100

    Fixed session controller endpoint.

--- Museum/Backend/REST service/src/main/java/museum/service/controllers/SessionController.java
+++ Museum/Backend/REST service/src/main/java/museum/service/controllers/SessionController.java
@@ -2,8 +2,6 @@ package museum.service.controllers;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,10 +9,10 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.servlet.http.HttpSession;
 
 @RestController
-@RequestMapping("/")
+@RequestMapping("/api/v1/")
 public class SessionController
 {
-    @GetMapping("login_status")
+    @PostMapping("login")
     public ResponseEntity checkSessionStatus()
     {
         return new ResponseEntity(HttpStatus.OK);
